9f635efd73d861a1ed6c66b4ff308eee23b304a3
Eduardo Sanchez <esanchez@Edu.local>
2019-10-17 19:56:49 -0300

    Crear objeto de memes

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 fetch('https://api.imgflip.com/get_memes')
 .then(response => {if (response.status === 200){
-        console.log("Successful request!")
+        console.log("Successful request!") 
     }else {
         console.log(`Oops, we get a ${response.status} error`)
     }
+    return response.json()
+})
+.then(info => {
+
+    console.log(info.data.memes)
+
+    let nuevoArray = []
+
+    info.data.memes.forEach(meme => {
+        
+        let newMemeItem = {
+            id: meme.id,
+            name: meme.name,
+            width: meme.width,
+            height: meme.height,
+            url: meme.url,
+        }
+
+        nuevoArray.push(newMemeItem)
+
+    });
+
+    console.log(nuevoArray)
+
 })
