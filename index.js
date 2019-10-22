a1c21bdc80a2d3b7e99787f0f1155078cc57032d
Eduardo Sanchez <esanchez@Edu.local>
2019-10-22 13:55:40 -0300

    Se modularizó las funciones, endpoint y fetch

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
-fetch('https://api.imgflip.com/get_memes')
-.then(response => {if (response.status === 200){
-        console.log("Successful request!") 
-    }else {
-        console.log(`Oops, we get a ${response.status} error`)
-    }
-    return response.json()
-})
-.then(info => {
+import { endpoint, traerMemes } from './api.js'
 
-    console.log(info.data.memes)
+  // Re-Estructuramos el objeto meme.
+function reestructurarArray (arrayMemes) {
 
-    let nuevoArray = []
+    let arrayMemeReconstruc = arrayMemes.map( meme => {
+        const {id, name, width, height, url} = meme
+        return {id, name, width, height, url}
+        }) 
 
-    info.data.memes.forEach(meme => {
-        
-        let newMemeItem = {
-            id: meme.id,
-            name: meme.name,
-            width: meme.width,
-            height: meme.height,
-            url: meme.url,
+    return arrayMemeReconstruc
+
+}
+
+  // Filtramos según criterio de medida
+function filtrar_y_Ordenar_Memes (array) {
+
+    let memesMenos500x500 = array.filter(function(meme){
+        if(meme.width >= 500 && meme.height >= 500){
+            return meme
         }
+    })
 
-        nuevoArray.push(newMemeItem)
+     memesMenos500x500.sort(function compare(a, b) {
+        return parseInt(a.id) - parseInt(b.id);
+      })
+     
+    console.log("organizados")
+    console.dir(memesMenos500x500)
+    return memesMenos500x500
+}
 
-    });
+function getMemeOfTheDay(arrayDeMemes) {
 
-    console.log(nuevoArray)
+    let dia = new Date().toJSON().slice(8,10)
+    console.log("El meme del día es...")
+    console.log(arrayDeMemes[parseInt(dia)])
+    return arrayDeMemes[parseInt(dia)]
+}
 
-})
+function getRandomMeme(arrayDeMemes) {
+
+    let numeroRandom = Math.round(Math.random() * arrayDeMemes.length-1)
+    console.log("El meme random es... " + numeroRandom)
+    console.log(arrayDeMemes[numeroRandom])
+    return arrayDeMemes[numeroRandom]
+}
+
+traerMemes(endpoint, reestructurarArray, filtrar_y_Ordenar_Memes, getMemeOfTheDay)
+traerMemes(endpoint, reestructurarArray, filtrar_y_Ordenar_Memes, getRandomMeme)
+
+
+
