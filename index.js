d4e658f17a2dfeb62840663ce3a5ab94166478e1
Eduardo Sanchez <esanchez@Edu.local>
2019-10-22 16:07:18 -0300

    styled frontend y funciones que lo modifican

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import { endpoint, traerMemes } from './api.js'
 
+let imagen_meme = document.getElementsByClassName("meme")[0]
+let h1 = document.querySelector("h1")
+let button = document.querySelector("button")
+let name = document.querySelector(".name")
+
   // Re-Estructuramos el objeto meme.
 function reestructurarArray (arrayMemes) {
 
@@ -35,6 +40,8 @@ function getMemeOfTheDay(arrayDeMemes) {
     let dia = new Date().toJSON().slice(8,10)
     console.log("El meme del día es...")
     console.log(arrayDeMemes[parseInt(dia)])
+   // esto se puede poner más bonito.
+    render(imagen_meme, "url", arrayDeMemes[parseInt(dia)].url)
     return arrayDeMemes[parseInt(dia)]
 }
 
@@ -43,11 +50,32 @@ function getRandomMeme(arrayDeMemes) {
     let numeroRandom = Math.round(Math.random() * arrayDeMemes.length-1)
     console.log("El meme random es... " + numeroRandom)
     console.log(arrayDeMemes[numeroRandom])
+    render(imagen_meme, "url", arrayDeMemes[numeroRandom].url)
+    render(name, "text", arrayDeMemes[numeroRandom].name)
+    render(h1, "text", "Random Meme!")
+    render(button, "text", "Get another random Meme!")
     return arrayDeMemes[numeroRandom]
 }
 
+//Trae al meme del día
 traerMemes(endpoint, reestructurarArray, filtrar_y_Ordenar_Memes, getMemeOfTheDay)
-traerMemes(endpoint, reestructurarArray, filtrar_y_Ordenar_Memes, getRandomMeme)
+
+
+function render (domElement, type, info) {
+
+    if(type == "url") {
+        domElement.src = info
+    } else if (type == "text") {
+        domElement.textContent = info
+    } else {
+        console.log("Falta el atributo")
+    } 
+}
+
+// Random Meme
+button.addEventListener("click", function(){
+    traerMemes(endpoint, reestructurarArray, filtrar_y_Ordenar_Memes, getRandomMeme)
+} )
 
 
 
